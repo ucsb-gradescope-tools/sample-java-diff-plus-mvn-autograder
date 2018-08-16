bbac98b7ea6d59659ab4e810440f29cbb8ffc1bb
Phill Conrad <pconrad@cs.ucsb.edu>
2018-08-16 16:28:02 -0700

    update sample solutions

---
+++ SAMPLE-SOLUTION-1/make-results.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -cp build/rational.jar:lib/junit-4.8.2.jar:lib/jh61b-1.0.jar RunTests
---
+++ SAMPLE-SOLUTION-2/src/Assignment.java
@@ -0,0 +1,19 @@
+public class Assignment {
+	public int myNumber;
+
+	public Assignment() {
+		myNumber = -42;
+	}
+
+	public Assignment(int n) {
+		myNumber = n;
+	}
+
+	public String hello() {
+		return "WRONG STRING world!";
+	}
+
+	public int add(int x) {
+		return myNumber - x;
+	}
+}
